813df81d92f286ef436dcf32ac11d5a7ea26253d
David Rogel <david.rogel.pernas@gmail.com>
2019-10-30 19:47:19 +0100

    added .gitignore to repo

---
+++ PiedPipers/Domain/Dummy/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  PiedPipers
+//
+//  Created by david rogel pernas on 30/10/2019.
+//  Copyright © 2019 david rogel pernas. All rights reserved.
+//
+
+import Foundation
+
+struct Dummy_Profile
+{
+    let name: String
+    let instruments: [String]
+    let videos: [String]
+    let description: String
+    let photo: String
+}
