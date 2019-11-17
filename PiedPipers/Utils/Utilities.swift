1036e4b666860ec1f79c67535134af4eda359ba4
David Rogel <david.rogel.pernas@gmail.com>
2019-11-17 22:12:56 +0100

    Añadido nuevo directorio de Utilidades y añada utilidad para formatear Precios

--- PiedPipers/Utils/Utilities.swift
+++ PiedPipers/Utils/Utilities.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+enum Fmt
+{
+    static func currency() -> NumberFormatter
+    {
+        return NumberFormatter()
+    }
+}
+
+// Esto sirve por ahora pero habría que extraerlo y montar una clase más bonika
+extension Double
+{
+    func toCurrency() -> String
+    {
+        let numberFormatter = NumberFormatter()
+        // para mostrar el punto o coma en los miles
+        numberFormatter.usesGroupingSeparator = true
+        // muestra siempre el dimal aunque sea un integer, ej: 100,00
+        numberFormatter.alwaysShowsDecimalSeparator = true
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale.current
+
+        guard let currencyStr = numberFormatter.string(from: NSNumber(value: self)) else
+        {
+            return String.init(format: "%.2f", locale: .current, self)
+        }
+        
+        return currencyStr
+    }
+}
+
+extension AnyHashable
+{
+    public static func == (lhs: AnyHashable, rhs: String) -> Bool
+    {
+        return lhs.description == rhs
+    }
+}
