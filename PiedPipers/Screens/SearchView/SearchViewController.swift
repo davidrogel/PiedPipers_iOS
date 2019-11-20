bfcdf38e2e3b965cfb2cb43eaf9473674b679ae0
David Rogel <david.rogel.pernas@gmail.com>
2019-11-21 00:03:59 +0100

    ViewController base para la SearchView

---
+++ PiedPipers/Screens/SearchView/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  PiedPipers
+//
+//  Created by david rogel pernas on 20/11/2019.
+//  Copyright © 2019 david rogel pernas. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
