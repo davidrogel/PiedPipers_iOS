908d0f784f41713b70e1b2b4022f481d6681faaf
David Rogel <david.rogel.pernas@gmail.com>
2019-12-15 13:23:05 +0100

    Creada clase para la carga de Imagenes y añadadia imagen para el placeholder (provisional) Añadido presenter para la vista de filtros para obtener los instrumentos Añadida vista para obtener los instrumentos con un Picker

---
+++ PiedPipers/Screens/SearchView/Presenter/FiltersViewPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  FiltersViewPresenter.swift
+//  PiedPipers
+//
+//  Created by david rogel pernas on 14/12/2019.
+//  Copyright © 2019 david rogel pernas. All rights reserved.
+//
+
+import Foundation
